5a7c9c0a22e827eb09843bbe082dd42cdb9e5624
mscorp-ce <m.s.copr@gmail.com>
2022-01-12 10:16:03 -0300

    Navigation buttons

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,8 +1,9 @@
+import { Link } from 'react-router-dom';
 import './styles.css';
 
 
 function Form() {
-  
+
   const movie = {
     id: 1,
     image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
@@ -35,7 +36,11 @@ function Form() {
             <button type="submit" className="btn btn-primary dsmovie-btn">Salvar</button>
           </div>
         </form >
-        <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+
+        <Link to="/" >
+          <button className="btn btn-primary dsmovie-btn mt-3">Cancelar</button>
+        </Link>
+
       </div >
     </div >
   );
