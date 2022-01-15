5aa684407e2bff4686375c4313fdfd4e7569a9ae
mscorp-ce <m.s.copr@gmail.com>
2022-01-15 05:00:46 -0300

    Security config

---
+++ backend/src/main/java/com/devsuperior/dsmovie/config/SecurityConfig.java
@@ -0,0 +1,7 @@
+package com.devsuperior.dsmovie.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className='container'>
                 <div className='dsmovie-nav-content'>
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/mscorp-ce">
+                    <a href="https://tmn.com.br/">
                         <div className='dsmovie-contact-container'>
                             <GithubIcon />
                             <p className='dsmovie-contact-link'>/mscorp-ce</p>
